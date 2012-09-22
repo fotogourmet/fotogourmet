f7cf6121e63f9f3f0079e05a34e6ddbb6e58a967
fotogourmet <fotogourmet.2012@gmail.com>
2012-09-22 17:11:58 -0300

    DataSource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -31,6 +31,19 @@ environments {
 		}
     }
     production {
-    
-	}
+		grails {
+			mongo {
+				host = "127.0.0.1"
+				port = 27017
+				//username = "app01"
+				//password = "app01"
+				databaseName = "test"
+				options {
+					autoConnectRetry = true
+					connectTimeout = 300
+
+					}
+			}
+		}
+    }
 }
