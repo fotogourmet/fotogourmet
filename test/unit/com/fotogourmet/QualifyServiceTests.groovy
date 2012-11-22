f5951287a9cc5d02d114b88358f46482ccefad1b
fotogourmet <fotogourmet.2012@gmail.com>
2012-11-21 21:42:43 -0300

    para el tio

---
+++ test/unit/com/fotogourmet/QualifyServiceTests.groovy
@@ -0,0 +1,19 @@
+package com.fotogourmet
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+import com.fotogourmet.qualify.QualifyService;
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(QualifyService)
+class QualifyServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
